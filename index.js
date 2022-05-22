1b2369d964e72c25dd81905c421ab4f42aa8e247
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2022-05-23 01:48:38 +0600

    all products load

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-// middleware
+// middleware--------
 app.use(cors());
 app.use(express.json());
 
-// mongodb connected
+// mongodb connected-----
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qw8fz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
   try{
     await client.connect();
-    console.log('Database connected')
+    const productCollection = client.db('machinery_parts').collection('products');
+    // all products load-----
+    app.get('/product', async(req, res) => {
+      const query = {};
+      const cursor  = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
   }
   finally{
 
